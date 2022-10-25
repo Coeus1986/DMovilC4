9ddedfb20d542feb862cf0df6fa317bb605eb108
Camilo Martinez <camo112233@gmail.com>
2022-10-24 22:14:36 -0500

    cree el esqueleto de codigo para SplashPage y para main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectc4/pages/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,52 +8,35 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Conocer.com',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-       
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-         
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
