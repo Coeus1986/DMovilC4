6571c937f933f041a3724dafa1ae393a3150318b
nanrhe <hdhh@hotmail.com>
2022-10-26 21:19:59 -0500

    Se crea la pagina para el login y la navegacion a la pagina registro, se actualiza archivo de configuracion para la ruta de las imagenes

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +11,38 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final user=TextEditingController();
+  final password=TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        width: 300,
+        child: ListView(
+          children: [
+
+            Container(
+              child:TextField(
+                controller: user,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.account_circle),
+                  hintText: "Usuario",
+                ),
+              )
+            ),
+            Container(
+              child: TextField(
+                obscureText: true,
+                controller: password,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    hintText: "Contraseña"
+                ),
+              )
+            )
+          ],
+        )
+      ),
+    );
   }
 }
