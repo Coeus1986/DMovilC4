3cf6b783fb18de14ee00f5778311a4fdd66cbbbc
nanrhe <hdhh@hotmail.com>
2022-11-08 19:55:33 -0500

    se configura el firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:projectc4/pages/register_page.dart';
 import 'package:projectc4/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
