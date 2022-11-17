8de31a1659a14dea47c0de55dffbdfa7242af0c7
nanrhe <hdhh@hotmail.com>
2022-11-17 18:36:47 -0500

    arreglo en modelo sitio turistico longitud latitud

--- lib/models/sitiosturisticos.dart
+++ lib/models/sitiosturisticos.dart
@@ -6,10 +6,13 @@ class SitiosTuristicos{
   String imagen="";
   String descripcion="";
   String temperatura="";
+  String latitud="";
+  String longitud="";
+
 
 
   SitiosTuristicos(this.lugar, this.resumen, this.ciudad, this.departamento,
-      this.imagen, this.descripcion, this.temperatura);
+      this.imagen, this.descripcion, this.temperatura,this.latitud,this.longitud);
 
   SitiosTuristicos.Empty();
 }
