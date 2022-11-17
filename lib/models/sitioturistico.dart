c549557dd05d1e4e2f9219cb83cb219f5a5551f8
Maikinu <mlopezsi@hotmail.com>
2022-11-16 22:25:34 -0500

    Configuración pagina lista sitios turisticos

---
+++ lib/models/sitioturistico.dart
@@ -0,0 +1,15 @@
+class SitioTuristico{
+  String lugar="";
+  String resumen="";
+  String ciudad="";
+  String departamento="";
+  String imagen="";
+  String descripcion="";
+  String temperatura="";
+
+
+  SitioTuristico(this.lugar, this.resumen, this.ciudad, this.departamento,
+      this.imagen, this.descripcion, this.temperatura);
+
+  SitioTuristico.Empty();
+}
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:projectc4/pages/detail_page.dart';
+
+import '../models/sitioturistico.dart';
 
 class ListsPage extends StatefulWidget {
   const ListsPage({super.key});
@@ -11,60 +14,61 @@ class ListsPage extends StatefulWidget {
 class _ListsPageState extends State<ListsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Lista de Sitios Turisticos"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lista de Sitios Turisticos"),
+        ),
+
+        body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection("sitioturistico").snapshots(),
+                builder: (context, snapshot){
+                  if(snapshot.hasError){
+                    return Text("Error en la consulta");
+                  }
+                  if(!snapshot.hasData){
+                    return Text("No existen datos");
+                  }
+
+                  child:
+                  return ListView.builder(
+                      itemCount: snapshot.data?.docs.length,
+                      itemBuilder: (context,index){
+                        QueryDocumentSnapshot sitioturistico=snapshot.data!.docs[index];
+
+                        return Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: Card(
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 30, top: 30),
+                                  child: ListTile(
+                                    title: Text(sitioturistico['lugar']),
+                                    subtitle: Text(sitioturistico['resumen']),
+                                    leading: Image.network(
+                                        sitioturistico['imagen']
+                                    ),
+                                    onTap: (){
+                                      //SitioTuristico sitioturistico=SitioTuristico(sitioturistico["lugar"], sitioturistico["apellido"], sitioturistico["correo"], sitioturistico["celular"], sitioturistico["imagen"], sitioturistico["descripcion"]);
+                                      Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage()));
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                  );
+                }
+            )
+        ),
       ),
-      body:ListView(
-      children: [
-      Container(
-        padding: EdgeInsets.all(5.0),
-      child: Text("Conoce con nosotros",
-      style: TextStyle(
-      fontSize: 24,
-      fontFamily: "titulo"
-      ),
-
-      ),
-      ),
-
-
-        ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => DetailPage())
-                );
-              },
-              child: ListTile(
-                title: Text("Piedra del Peñol"),
-                subtitle: Text("Conoce la Piedra del Peñol icono del turismo de Antioquia"),
-                leading: GestureDetector(
-                  behavior: HitTestBehavior.translucent,
-                  onTap: () {},
-                  child: Container(
-                    width: 58,
-                    height: 58,
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    alignment: Alignment.center,
-                    child: const CircleAvatar(
-                      backgroundImage: AssetImage("assets/images/guatape.png"
-                      ),
-                    ),
-                  ),
-                ),
-            ),
-
-        )
-
-      ],
-    ),
     );
-
-
   }
 }
